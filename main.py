13ea4da3cfd5697063a4f2c9a9fed2d5ad6f5bb0
stevenzhang124 <1241922639@qq.com>
2020-09-22 20:48:35 +0800

    init

---
+++ main.py
@@ -0,0 +1,71 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+from torchvision import datasets, transforms
+import torch
+
+from utils.sampling import load_data, data_iid, data_noniid
+from utils.options import args_parser
+from utils.Fed import FedAvg
+from models.Update import LocalUpdate
+from models import models
+
+if __name__ == '__main__':
+    #parse args
+    args = args_parser()
+    args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
+    torch.manual_seed(args.seed)
+
+    #load dataset and distribute to users
+    source_name = "amazon"
+    target_name = "webcam"
+
+    print('Src: %s, Tar: %s' % (source_name, target_name))
+    source_data, target_train_data, target_test_data = load_data(source_name, target_name, data_dir='dataset/')
+
+    if args.iid:
+        dict_users = data_iid(target_train_data, args.num_users)
+    else:
+        dict_users = data_noniid(target_train_data, args.num_users)
+
+    #load global model
+    model = models.Transfer_Net(
+        args.n_class, transfer_loss='mmd', base_net='resnet50').to(args.device)
+    
+
+    #copy weights
+    w_glob = model.state_dict()
+
+    #training
+    loss_train = []
+
+    for iter in range(args.epochs):
+        w_locals, loss_locals = [], []
+        m = max(int(args.frac * args.num_users), 1)
+        idxs_users = np.random.choice(range(args.num_users), m, replace=False)
+        for idx in idxs_users:
+            local = LocalUpdate(args=args, source_data, target_train_data, target_test_data, idxs=dict_users[idx])
+            w, loss = local.train(copy.deepcopy(model).to(args.device))
+            w_locals.append(copy.deepcopy(w))
+            loss_locals.append(copy.deepcopy(loss))
+        # update global weights
+        w_glob = FedAvg(w_locals)
+
+        # copy weight to global model
+        model.load_state_dict(w_glob)
+
+        # print loss
+        loss_avg = sum(loss_locals) / len(loss_locals)
+        print('Round {:3d}, Average loss {:.3f}'.format(iter, loss_avg))
+        loss_train.append(loss_avg)
+    
+    # plot loss curve
+    plt.figure()
+    plt.plot(range(len(loss_train)), loss_train)
+    plt.ylabel('train_loss')
+    plt.savefig('./save/fed_{}_{}_{}_C{}_iid{}.png'.format(source_name, target_name, args.epochs, args.frac, args.iid))
+
+    #testing
+    local.test(model.to(args.device))
---
+++ models/Update.py
@@ -0,0 +1,77 @@
+import torch
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+import random
+
+
+
+class DatasetSplit(Dataset):
+    def __init__(self, dataset, idxs):
+        self.dataset = dataset
+        self.idxs = list(idxs)
+
+    def __len__(self):
+        return len(self.idxs)
+
+    def __getitem__(self, item):
+        image, label = self.dataset[self.idxs[item]]
+        return image, label
+
+class LocalUpdate(object):
+    def __init__(self, args, source, target_train, target_test, idxs=None):
+        self.args = args
+        self.source_loader = DataLoader(DatasetSplit(source, idxs), batch_size=self.args.local_bs, shuffle=True, num_workers=4)
+        self.target_train_loader = DataLoader(DatasetSplit(target_train, idxs), batch_size=self.args.local_bs, shuffle=True, num_workers=4)
+        self.target_test_loader = DataLoader(target_test, batch_size=self.args.bs, shuffle=True, num_workers=4)
+
+    def train(self, model):
+        optimizer = torch.optim.SGD([
+            {'params': model.base_network.parameters()},
+            {'params': model.bottleneck_layer.parameters(), 'lr': 10 * self.args.lr},
+            {'params': model.classifier_layer.parameters(), 'lr': 10 * self.args.lr},
+            ], lr=self.args.lr, momentum=self.args.momentum, weight_decay=self.args.l2_decay)
+
+        len_source_loader = len(self.source_loader)
+        len_target_loader = len(self.target_train_loader)
+
+        epoch_loss = []
+        for e in range(self.args.local_ep):
+            batch_loss = []
+            model.train()
+            iter_source, iter_target = iter(
+                self.source_loader), iter(self.target_train_loader)
+            n_batch = min(len_source_loader, len_target_loader)
+            criterion = torch.nn.CrossEntropyLoss()
+            for i in range(n_batch):
+                data_source, label_source = iter_source.next()
+                data_target, _ = iter_target.next()
+                data_source, label_source = data_source.to(
+                    self.args.device), label_source.to(self.args.device)
+                data_target = data_target.to(self.args.device)
+
+                optimizer.zero_grad()
+                label_source_pred, transfer_loss = model(data_source, data_target)
+                clf_loss = criterion(label_source_pred, label_source)
+                loss = clf_loss + self.args.lam * transfer_loss
+                loss.backward()
+                optimizer.step()
+                
+                batch_loss.append(loss.item())
+            epoch_loss.append(sum(batch_loss)/len(batch_loss))
+            return model.state_dict(), sum(epoch_loss) / len(epoch_loss)
+            
+
+    def test(self, model):
+        model.eval()
+        correct = 0
+        #criterion = torch.nn.CrossEntropyLoss()
+        len_target_dataset = len(self.target_test_loader.dataset)
+        with torch.no_grad():
+            for data, target in self.target_test_loader:
+                data, target = data.to(self.args.device), target.to(self.args.device)
+                s_output = model.predict(data)
+                #loss = criterion(s_output, target)
+                pred = torch.max(s_output, 1)[1]
+                correct += torch.sum(pred == target)
+
+        print('max correct: {}, accuracy{: .2f}%\n'.format(correct, 100. * correct / len_target_dataset))
