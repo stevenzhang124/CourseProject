3aae07e2e3751bd0730a36a0925a1bc61913f807
sxontheway <sxontheway@users.noreply.github.com>
2020-11-03 21:07:34 +0800

    update main

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
-from model import models
-from model.Update import LocalUpdate
-from utils.Fed import FedAvg
-from utils.options import args_parser
-from utils.sampling import load_data, data_iid, data_noniid
-import torch
-from torchvision import datasets, transforms
-import numpy as np
-import copy
-import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+from torchvision import datasets, transforms
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+from utils.sampling import load_data, data_iid, data_noniid
+from utils.options import args_parser
+from utils.Fed import FedAvg
+from model.Update import LocalUpdate
+from model import models
 
 
 if __name__ == '__main__':
     # parse args
     args = args_parser()
-    args.device = torch.device('cuda:{}'.format(
-        args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
+    args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
     torch.manual_seed(args.seed)
 
     # load dataset and distribute to users
-    source_name = "webcam"
-    target_name = "amazon"
+    source_name = "amazon"
+    target_name = "webcam"
 
     print('Src: %s, Tar: %s' % (source_name, target_name))
-    source_data, target_train_data, target_test_data = load_data(
-        source_name, target_name, data_dir='dataset/')
+    source_data, target_train_data, target_test_data = load_data(source_name, target_name, data_dir='/data/xian/Office-31/')
 
-    if args.iid:
+    if args.iid:    # default false
         dict_users = data_iid(target_train_data, args.num_users)
     else:
         dict_users = data_noniid(target_train_data, args.num_users)
@@ -35,44 +35,56 @@ if __name__ == '__main__':
     # load global model
     model = models.Transfer_Net(
         args.n_class, transfer_loss='mmd', base_net='resnet18').to(args.device)
+    
 
     # copy weights
     w_glob = model.state_dict()
 
     # training
     loss_train = []
+    best_acc = 0
 
-    for iter in range(args.epochs):
-        w_locals, loss_locals = [], []
-        m = max(int(args.frac * args.num_users), 1)
-        idxs_users = np.random.choice(range(args.num_users), m, replace=False)
-        j = 0
-        for idx in idxs_users:
-            local = LocalUpdate(
-                args, source_data, target_train_data, target_test_data, idxs=dict_users[idx])
-            j = j + 1
-            print(j)
-            w, loss = local.train(copy.deepcopy(model).to(args.device))
-            w_locals.append(copy.deepcopy(w))
-            loss_locals.append(copy.deepcopy(loss))
+    with SummaryWriter() as writer:
+        for epoch in range(args.rounds):
+            w_locals, loss_locals = [], []
+            m = max(int(args.frac * args.num_users), 1)
+            idxs_users = np.random.choice(range(args.num_users), m, replace=False)
+            j=0
+            for idx in idxs_users:
+                local = LocalUpdate(args, source_data, target_train_data, target_test_data, idxs=dict_users[idx])
+                print(j)
+                j = j + 1
+                w, loss, optimizer = local.train(copy.deepcopy(model).to(args.device))
+                w_locals.append(copy.deepcopy(w))
+                loss_locals.append(copy.deepcopy(loss))
 
-        # update global weights
-        w_glob = FedAvg(w_locals)
+            # update global weights
+            w_glob = FedAvg(w_locals)
 
-        # copy weight to global model
-        model.load_state_dict(w_glob)
+            # copy weight to global model
+            model.load_state_dict(w_glob)
 
-        # print loss
-        loss_avg = sum(loss_locals) / len(loss_locals)
-        print('Round {:3d}, Average loss {:.3f}'.format(iter, loss_avg))
-        loss_train.append(loss_avg)
+            # print loss
+            loss_avg = sum(loss_locals) / len(loss_locals)
+            print('Round {:3d}, Average loss {:.3f}'.format(epoch, loss_avg))
+            loss_train.append(loss_avg)
+            writer.add_scalar("loss_avg", loss_avg, global_step = epoch)
 
-    # plot loss curve
-    plt.figure()
-    plt.plot(range(len(loss_train)), loss_train)
-    plt.ylabel('train_loss')
-    plt.savefig('./save/fed_{}_{}_{}_C{}_iid{}.png'.format(source_name,
-                                                           target_name, args.epochs, args.frac, args.iid))
+            # evaluation and save
+            if epoch % args.eval_interval == 0:
+                # evaluate
+                acc = local.test(model.to(args.device))
+                writer.add_scalar("accuracy", acc, global_step = epoch)
+                # save last, best and delete
+                ckpt = {'epoch': epoch, 'model': model.state_dict(), 'optimizer': optimizer}
+                torch.save(ckpt, f"./save/last_iid{args.iid}.pt")
+                if acc > best_acc  and epoch < args.rounds-1:
+                    torch.save(ckpt, f"./save/best_iid{args.iid}.pt")
+                    best_acc = acc
+                del ckpt
 
-    # testing
-    local.test(model.to(args.device))
+        # plot loss curve
+        plt.figure()
+        plt.plot(range(len(loss_train)), loss_train)
+        plt.ylabel('train_loss')
+        plt.savefig('./save/fed_{}_{}_{}_C{}_iid{}.png'.format(source_name, target_name, args.rounds, args.frac, args.iid))
